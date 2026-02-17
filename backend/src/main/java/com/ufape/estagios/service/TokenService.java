33ba7d3d60daaca87eef9e3c69d71bde2197cf28
Pedro Henrique <pedrohenriquemo2020@gmail.com>
2026-02-17 19:11:24 -0300

    Adds user role to JWT claims

--- backend/src/main/java/com/ufape/estagios/service/TokenService.java
+++ backend/src/main/java/com/ufape/estagios/service/TokenService.java
@@ -23,6 +23,7 @@ public class TokenService {
     public String generateToken(Usuario usuario) {
         return Jwts.builder()
                 .setSubject(usuario.getEmail())
+                .claim("role", usuario.getRole().name())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
