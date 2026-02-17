32148c61fa5eb25e0d3aaeb1b847949d428a07a6
Pedro Henrique <pedrohenriquemo2020@gmail.com>
2026-02-17 19:11:01 -0300

    Refactors VagaController mapping

--- backend/src/main/java/com/ufape/estagios/controller/VagaController.java
+++ backend/src/main/java/com/ufape/estagios/controller/VagaController.java
@@ -1,18 +1,27 @@
 package com.ufape.estagios.controller;
 
-import com.ufape.estagios.dto.VagaRequestDTO;
-import com.ufape.estagios.dto.VagaResponseDTO;
-import com.ufape.estagios.service.VagaService;
-import jakarta.validation.Valid;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.ufape.estagios.dto.VagaRequestDTO;
+import com.ufape.estagios.dto.VagaResponseDTO;
+import com.ufape.estagios.service.VagaService;
+
+import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/vagas")
+@RequestMapping("api/vagas")
 public class VagaController {
 
     @Autowired
